367293f943ad08dc3e45846da3465ab378262a3e
Agatha Saez <asaez@rayensalud.com>
2020-12-03 16:10:00 -0300

    intalo webpack para manejo de archivos de imagen; creo carpeta en assets dónde agrego imagenes; creo footer; paso title para cada categoría de forma dinámica.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
+
+/* Styles */
+import '../assets/styles/App.scss';
+
 /* Components */
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Categories from '../components/Categories';
-
-/* Styles */
-import '../assets/styles/App.scss';
+import Footer from '../components/Footer';
 
 const App = () => (
     <div className="App">
-        <Header/>
-        <Search/>
+        <Header />
+        <Search />
 
-        <Categories>
+        <Categories title="Seguir viendo">
             <Carousel>
                 <CarouselItem />  
                 <CarouselItem />  
                 <CarouselItem />  
             </Carousel>    
-        </Categories>       
+        </Categories> 
+
+        <Categories title="Mi lista">
+            <Carousel>
+                <CarouselItem />  
+                <CarouselItem />  
+                <CarouselItem />  
+            </Carousel>    
+        </Categories> 
+
+        <Categories title="Populares">
+            <Carousel>
+                <CarouselItem />  
+                <CarouselItem />  
+                <CarouselItem />  
+            </Carousel>    
+        </Categories> 
+
+        <Footer />      
     </div>
 )
 
